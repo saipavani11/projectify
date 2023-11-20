94d4bddd994abb329f48fa583a2a0fca1962fe6f
saipavani11 <saipavani2004@gmail.com>
2023-11-20 18:17:34 +0530

    made it responsive

--- script.js
+++ script.js
@@ -37,22 +37,31 @@ function displayVeg(event){
     }
 }
 
+let menulist = document.querySelector('.menuList');
+let menuicon = document.getElementById("menu-icon");
+let dropdown = document.querySelector('.dropdown');
 
-// // let sections = document.querySelectorAll('section');
-// // let navlinks = document.querySelectorAll('header nav div a');
+menulist.onclick = function(){
+    dropdown.classList.toggle('open');
+    const isOpen = dropdown.classList.contains('open');
+}
 
-// // window.onscroll = () => {
-// //     sections.forEach(sec => {
-// //         let top = window.scrollY;
-// //         let offset = sec.offsetTop;
-// //         let height = sec.offsetHeight;
-// //         let id = sec.getAttribute('id');
 
-// //         if(top >= offset && top < offset+height){
-// //             navlinks.forEach(links => {
-// //                 links.classlist.remove('active');
-// //                 document.querySelector('header nav a [href*=' + id + ']').classList.add('active');
-// //             })
-// //         }
-// //     })
-// // }
+// let sections = document.querySelectorAll('section');
+// let navlinks = document.querySelectorAll('header nav div ul li a');
+
+// window.onscroll = () => {
+//     sections.forEach(sec => {
+//         let top = window.scrollY;
+//         let offset = sec.offsetTop;
+//         let height = sec.offsetHeight;
+//         let id = sec.getAttribute('id');
+
+//         if(top >= offset && top < offset+height){
+//             navlinks.forEach(links => {
+//                 links.classlist.remove('active');
+//                 document.querySelector('header nav ul li a [href*=' + id + ']').classList.add('active');
+//             })
+//         }
+//     })
+// }
