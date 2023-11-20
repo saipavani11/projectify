a375c7c8a30b568772c5e3859904ec3c52ae87af
Snehil-Kashyap <snehilk31@gmail.com>
2023-11-20 21:58:02 +0530

    hidden class added

--- script.js
+++ script.js
@@ -2,25 +2,24 @@
 let Vegs = document.getElementById("vegs");
 let nonVegs = document.getElementById("non-vegs");
 // nonVegs.style.display = "none";
-nonVegs.classList.add('hidden');
+nonVegs.classList.add("hidden");
 console.log(nonVegs.classList);
-function displayVegs(event){
-    var text = event.innerText || event.textContent;
-    if(text == 'Go To Non-Veg'){
-        // Vegs.style.display = "none";
-        console.log('high')
-        Vegs.classList.add('hidden');
-        nonVegs.classList.remove('hidden')
-        // nonVegs.style.display = "flex";
-        event.innerHTML = 'Go To Veg';
-
-    }else{
-        Vegs.classList.remove('hidden');
-        nonVegs.classList.add('hidden')
-        // Vegs.style.display = "flex";
-        // nonVegs.style.display = "none";
-        event.innerHTML = 'Go To Non-Veg';
-    }
+function displayVegs(event) {
+  var text = event.innerText || event.textContent;
+  if (text == "Go To Non-Veg") {
+    // Vegs.style.display = "none";
+    console.log("high");
+    Vegs.classList.add("hidden");
+    nonVegs.classList.remove("hidden");
+    // nonVegs.style.display = "flex";
+    event.innerHTML = "Go To Veg";
+  } else {
+    Vegs.classList.remove("hidden");
+    nonVegs.classList.add("hidden");
+    // Vegs.style.display = "flex";
+    // nonVegs.style.display = "none";
+    event.innerHTML = "Go To Non-Veg";
+  }
 }
 
 //for maincourse
@@ -29,29 +28,27 @@ let Veg = document.getElementById("veg");
 let nonVeg = document.getElementById("non-veg");
 nonVeg.style.display = "none";
 
-function displayVeg(event){
-    var text = event.innerText || event.textContent;
-    if(text == 'Go To Non-Veg'){
-        Veg.style.display = "none";
-        nonVeg.style.display = "flex";
-        event.innerHTML = 'Go To Veg';
-
-    }else{
-        Veg.style.display = "flex";
-        nonVeg.style.display = "none";
-        event.innerHTML = 'Go To Non-Veg';
-    }
+function displayVeg(event) {
+  var text = event.innerText || event.textContent;
+  if (text == "Go To Non-Veg") {
+    Veg.style.display = "none";
+    nonVeg.style.display = "flex";
+    event.innerHTML = "Go To Veg";
+  } else {
+    Veg.style.display = "flex";
+    nonVeg.style.display = "none";
+    event.innerHTML = "Go To Non-Veg";
+  }
 }
 
-let menulist = document.querySelector('.menuList');
+let menulist = document.querySelector(".menuList");
 let menuicon = document.getElementById("menu-icon");
-let dropdown = document.querySelector('.dropdown');
+let dropdown = document.querySelector(".dropdown");
 
-menulist.onclick = function(){
-    dropdown.classList.toggle('open');
-    const isOpen = dropdown.classList.contains('open');
-}
-
+menulist.onclick = function () {
+  dropdown.classList.toggle("open");
+  const isOpen = dropdown.classList.contains("open");
+};
 
 // let sections = document.querySelectorAll('section');
 // let navlinks = document.querySelectorAll('header nav div ul li a');
